376ddf21a5a5ddbff1122eaa65c002f3fd9e5da2
Steven Riehl <sr@stevenriehl.com>
2022-12-04 10:08:40 -0700

    Day 04 done

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,43 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  defp parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn ranges ->
+      {:ok, [a1, a2, b1, b2], _, _, _, _} = AdventOfCode.Day04.Parse.ranges(ranges)
+      {a1, a2, b1, b2}
+    end)
   end
 
-  def part2(_args) do
+  defp full_overlap(a1, a2, b1, b2) when a1 >= b1 and a2 <= b2, do: true
+  defp full_overlap(a1, a2, b1, b2) when b1 >= a1 and b2 <= a2, do: true
+  defp full_overlap(_, _, _, _), do: false
+
+  def part1(input) do
+    input
+    |> parse_input()
+    |> Enum.map(fn {a1, a2, b1, b2} -> full_overlap(a1, a2, b1, b2) end)
+    |> Enum.count(fn bool -> bool end)
   end
+
+  def part2(input) do
+    input
+    |> parse_input()
+    |> Enum.map(fn {a1, a2, b1, b2} -> !Range.disjoint?(a1..a2, b1..b2) end)
+    |> Enum.count(fn bool -> bool end)
+  end
+end
+
+defmodule AdventOfCode.Day04.Parse do
+  import NimbleParsec
+
+  ranges =
+    integer(min: 1)
+    |> ignore(string("-"))
+    |> integer(min: 1)
+    |> ignore(string(","))
+    |> integer(min: 1)
+    |> ignore(string("-"))
+    |> integer(min: 1)
+
+  defparsec(:ranges, ranges)
 end
