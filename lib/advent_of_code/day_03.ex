be3a28551de736110e3aaa8dd983d76116552b35
Steven Riehl <sr@stevenriehl.com>
2022-12-04 09:44:33 -0700

    Day 3 done

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,41 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  defp parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_charlist/1)
   end
 
-  def part2(_args) do
+  defp score_letter(letter) when letter >= 97, do: letter - ?a + 1
+  defp score_letter(letter), do: letter - ?A + 27
+
+  def part1(input) do
+    input
+    |> parse_input()
+    |> Enum.map(fn rucksack ->
+      len = rucksack |> length() |> div(2)
+      first = Enum.slice(rucksack, 0..(len - 1))
+      second = Enum.slice(rucksack, len..-1)
+      MapSet.intersection(MapSet.new(first), MapSet.new(second))
+    end)
+    |> Enum.flat_map(&MapSet.to_list/1)
+    |> Enum.map(&score_letter/1)
+    |> Enum.sum()
+  end
+
+  def part2(input) do
+    input
+    |> parse_input()
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn group ->
+      group = Enum.map(group, &MapSet.new/1)
+      a = Enum.at(group, 0)
+      b = Enum.at(group, 1)
+      c = Enum.at(group, 2)
+      shared = MapSet.intersection(a, b)
+      MapSet.intersection(shared, c)
+    end)
+    |> Enum.flat_map(&MapSet.to_list/1)
+    |> Enum.map(&score_letter/1)
+    |> Enum.sum()
   end
 end
